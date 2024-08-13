ffa6d2d0f8dad9c35ee6410bf3eb99723a8b8ee5
azhou0770 <azhou0770@gmail.com>
2024-08-13 13:14:52 -0700

    Implemented a bit of the RenterModel Service layer.

---
+++ backend/src/main/java/com/revature/services/RenterService.java
@@ -0,0 +1,13 @@
+package com.revature.services;
+
+import com.revature.models.RenterModel;
+import com.revature.models.UserModel;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface RenterService {
+    public RenterModel createRenter(UserModel user);
+    public List<RenterModel> getBookings();
+}
---
+++ backend/src/main/java/com/revature/services/RenterServiceImpl.java
@@ -0,0 +1,25 @@
+package com.revature.services;
+
+import com.revature.models.RenterModel;
+import com.revature.models.UserModel;
+import com.revature.repositories.RenterRepo;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class RenterServiceImpl implements RenterService{
+    RenterRepo renterRepo;
+    @Override
+    public RenterModel createRenter(UserModel user){
+        RenterModel renter = new RenterModel(user);
+        return renterRepo.save(renter);
+    }
+//    @Override
+//    public List<RenterModel> getBookings(long id){
+//        RenterModel renter = renterRepo.findById(id);
+//        return renter.getBookings();
+//    }
+}
