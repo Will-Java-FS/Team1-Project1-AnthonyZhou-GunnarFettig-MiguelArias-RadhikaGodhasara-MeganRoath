253bb6470431ccfa72c6a0962f0314c86f150e89
azhou0770 <azhou0770@gmail.com>
2024-08-15 12:47:30 -0700

    Added owner and renter service and controller implementation. Commented some things out where I needed BookingModel and PropertyModel

--- backend/src/main/java/com/revature/controllers/OwnerController.java
+++ backend/src/main/java/com/revature/controllers/OwnerController.java
@@ -1,4 +1,29 @@
 package com.revature.controllers;
 
+import com.revature.services.OwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 public class OwnerController {
+
+    @Autowired
+    private OwnerService ownerService;
+
+    // Register a new owner
+    // Param will be an Owner JSON
+//    @PostMapping("/register")
+//    public ResponseEntity<OwnerModel> registerOwner(@RequestBody OwnerModel owner) {
+//        OwnerModel owner = ownerService.registerOwner(owner);
+//        return ResponseEntity.ok(owner);
+//    }
+
+    // Get properties for a specific owner
+    // Param should be a JWT token probably?
+//    @GetMapping("/properties")
+//    public ResponseEntity<List<PropertyModel>> getPropertiesForOwner() {
+//        List<PropertyModel> properties = ownerService.getPropertiesForOwner();
+//        return ResponseEntity.ok(properties);
+//    }
+
 }
--- backend/src/main/java/com/revature/services/RenterService.java
+++ backend/src/main/java/com/revature/services/RenterService.java
@@ -1,6 +1,6 @@
 package com.revature.services;
 
-import com.revature.models.BookingsModel;
+//import com.revature.models.BookingsModel;
 import com.revature.models.RenterModel;
 import com.revature.models.UserModel;
 import org.springframework.stereotype.Service;
@@ -10,5 +10,5 @@ import java.util.List;
 @Service
 public interface RenterService {
     public RenterModel createRenter(UserModel user);
-    public List<BookingsModel> getBookings(long id);
+//    public List<BookingsModel> getBookingsForRenter(long id);
 }
