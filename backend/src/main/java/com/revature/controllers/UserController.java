ef93ed7ebe5798b5492507b6dd9676b0d870a97a
RadhikaGodhasara <radhikagdesigner@gmail.com>
2024-08-20 13:40:42 -0700

    authenticate

--- backend/src/main/java/com/revature/controllers/UserController.java
+++ backend/src/main/java/com/revature/controllers/UserController.java
@@ -1,13 +1,13 @@
 package com.revature.controllers;
 
-
-
 import com.revature.WebToken.JwtService;
 import com.revature.WebToken.LoginForm;
-
+import com.revature.models.AuthResponse;
 import com.revature.models.UserModel;
 import com.revature.repositories.UserRepo;
 import com.revature.services.UserModelDetailService;
+
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,8 +16,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
 
     @Autowired
@@ -38,19 +38,36 @@ public class UserController {
     }
 
     @PostMapping("/authenticate")
-    public String authAndGetToken(@RequestBody LoginForm loginForm){
-    Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-            loginForm.username(),loginForm.password()
-    ));
-        if (authentication.isAuthenticated()){
-       return jwtService.generateToken(userModelDetailService.loadUserByUsername(loginForm.username()));
-         }else {
-            throw new UsernameNotFoundException("invalid");
+    // public String authAndGetToken(@RequestBody LoginForm loginForm){
+
+    //     Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+    //         loginForm.username(),loginForm.password()
+    //     ));
+
+    //     if (authentication.isAuthenticated()){
+    //         return jwtService.generateToken(userModelDetailService.loadUserByUsername(loginForm.username()));
+    //     }else {
+    //         throw new UsernameNotFoundException("invalid");
+    //     }
+    // }
+    public AuthResponse authAndGetToken(@RequestBody LoginForm loginForm){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+            loginForm.username(), loginForm.password()
+        ));
+
+        if(authentication.isAuthenticated()){
+            Optional<UserModel> optionalUser = userRepo.findByUsername(loginForm.username());
+            UserModel user = optionalUser.get();
+            String token = jwtService.generateToken(userModelDetailService.loadUserByUsername(loginForm.username()));
+
+            if(optionalUser.isEmpty()){
+                throw new UsernameNotFoundException("User not found");
+            }
+
+            return new AuthResponse(user.getUsername(), user.getId(), user.getRole(), token);
+        }
+        else{
+            throw new UsernameNotFoundException("Invalid credentials");
         }
     }
-
-
-
-
-
 }
