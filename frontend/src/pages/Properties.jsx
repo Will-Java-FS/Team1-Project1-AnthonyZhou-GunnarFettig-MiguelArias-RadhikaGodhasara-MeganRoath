cfc2b5b7e35e7b32bcfc3073e73b5645d5ec5cc3
gufe2213 <gufe2213@colorado.edu>
2024-08-21 17:22:52 -0600

    Checkbox fix

--- frontend/src/pages/Properties.jsx
+++ frontend/src/pages/Properties.jsx
@@ -135,7 +135,7 @@ export default function Properties() {
                             </NumberInputStepper>
                         </NumberInput>
                     </FormControl>
-                    <FormControl isRequired>
+                    <FormControl>
                         <FormLabel>Pets</FormLabel>
                         <Checkbox isChecked={pets} onChange={(event) => setPets(event.target.checked)}>Allow Pets</Checkbox>
                     </FormControl>
